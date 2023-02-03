88e35a3cefbab8909dc30d8d8bb0ea4268b7fe57
Skikamika1990 <106681407+Skikamika1990@users.noreply.github.com>
2023-02-03 11:14:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,12 @@ echo "Open script ..."
 curl -fsSL https://get.docker.com -o get-docker.sh && \
 sh get-docker.sh && \
 rm -rf get-docker.sh
-echo "Finish..."
 
-echo "Prepering Docker..."
 usermod -aG docker $Skika
 
 systemctl enable docker
 systemctl start docker
 
-echo "Downloading docker-compose.yaml..."
 curl https://www.espressosys.com/cape/docker-compose.yaml --output docker-compose.yaml
 echo "The file was downloaded..."
 
@@ -22,4 +19,4 @@ docker compose pull
 docker compose up -d
 echo "Docker started..."
 
-echo "Happy End."
+echo "Happy End"
